632c4838b6f35374699195a16fa8ad4d782318db
Babayaga <nayanpa088@gmail.com>
2021-05-30 18:31:26 +0530

    stash apply

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ def add(x,y):
 	return (x+y) #master did this
 
 def sub(x,y):
+<<<<<<< Updated upstream
 	return x-y #master sub
+=======
+	return (x*y) #(stage) master did this
+>>>>>>> Stashed changes
 
 def mul(x,y):
 
